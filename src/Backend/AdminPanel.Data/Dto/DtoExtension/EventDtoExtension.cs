b8a6c23b4170aa9e6cdeca04405d3d355c9f5642
Mihkail Afanasev <afanasev10056@gmail.com>
2023-11-23 13:13:06 +0300

    add dto and mapper for event entity

---
+++ src/Backend/AdminPanel.Data/Dto/DtoExtension/EventDtoExtension.cs
@@ -0,0 +1,18 @@
+﻿using AdminPanel.Data.Entity;
+
+namespace AdminPanel.Data.Dto.DtoExtension
+{
+    public static class EventDtoExtension
+    {
+        public static EventDto ToDto(this Event myEvent)
+        {
+            return new EventDto
+            {
+                Id = myEvent.Id,
+                EventDate = myEvent.EventDate,
+                EventName = myEvent.EventName,
+                ImagePath = myEvent.ImagePath
+            };
+        }
+    }
+}
---
+++ src/Backend/AdminPanel.Data/Dto/EventDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AdminPanel.Data.Dto;
+
+public class EventDto
+{
+    public int Id { get; set; }
+
+    public string EventName { get; set; } = null!;
+
+    public DateTime EventDate { get; set; }
+    public string? ImagePath { get; set; }
+}
